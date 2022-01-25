35ef425dfc270d7f4c94579076282c1ff09d3ef1
unknown <vitalii.kiktov@gmail.com>
2022-01-25 16:29:54 +0300

    feat: add img cashe

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,12 @@
+import i18Obj from './translate.js';
+
+console.log(i18Obj);
+
 const allButtons = document.querySelectorAll('.portfolio-button');
 const portfolioImages = document.querySelectorAll('.portfolio__img');
 const portfolioBtns = document.querySelector('.portfolio__list');
 const hamburger = document.querySelector('.burger-menu');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
 // button current switch
 
@@ -22,7 +27,6 @@ portfolioBtns.addEventListener('click', function changeImage(event) {
 
   else if(event.target.dataset.season == "spring") {
     portfolioImages.forEach((img, index) => img.src = `./assets/img/spring/${index + 1}.jpg`);
-    portfolioBtns.classList.add('active');
   }
 
   else if(event.target.dataset.season == "summer") {
@@ -44,25 +48,19 @@ function toggleMenu() {
 hamburger.addEventListener('click', toggleMenu);
 
 
+// image cashe
 
 
-$(function() {  
-  $('.button')
-    .on('mouseenter', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-			$(this).find('span').css({top:relY, left:relX})
-    })
-    .on('mouseout', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-    	$(this).find('span').css({top:relY, left:relX})
+function preloadImages() {
+  for(let i = 1; i <= 6; i++) {
+    seasons.forEach((item) => {
+      const img = new Image();
+      img.src = `./assets/img/${item}/${i}.jpg`;
     });
-  $('[href=#]').click(function(){return false});
-});
-
+    
+  }
+}
+preloadImages();
 
 
 console.log("-Вёрстка соответствует макету. Ширина экрана 768px +48\n\n-Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n\n-На ширине экрана 768рх и меньше реализовано адаптивное меню +22")

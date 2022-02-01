c08c77f76af2f2f178230b5864a74f0088fe3009
5t4b3r <vitalii.kiktov@gmail.com>
2022-02-01 13:36:58 +0300

    feat: add html and style page

---
+++ eco-sounds/index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.burger-menu');
+
+
+// // burger menu toggle button
+
+hamburger.addEventListener('click', function toggleMenu() {
+    hamburger.classList.toggle('burger-menu_active');
+  });

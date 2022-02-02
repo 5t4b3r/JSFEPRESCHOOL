b00458b44932a71781b5027dd188a4a33a6df6fd
5t4b3r <vitalii.kiktov@gmail.com>
2022-02-02 22:37:30 +0300

    fix: burger menu play sound

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,12 +1,12 @@
-
 const navigationButton = document.querySelectorAll(".navigation__item");
 const allButton = document.querySelector(".navigation__list");
 const mainImg = document.querySelector(".main-index");
 const playerButton = document.querySelector(".player-button");
 const audio = document.querySelector("audio");
-const progress = document.querySelector(".progress")
-const progressContainer = document.querySelector('.progress-container')
+const progress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
 const songs = ["forest", "solovey", "drozd", "javoronok", "zarynka", "slavka"];
+const burgerList = document.querySelector(".burger-menu__nav_list");
 
 // burger menu toggle button
 const hamburger = document.querySelector(".burger-menu");
@@ -32,7 +32,7 @@ let songIndex = 0;
 
 function loadSong(song) {
   audio.src = `./assets/audio/${song}.mp3`;
-};
+}
 
 loadSong(songs[songIndex]);
 
@@ -57,25 +57,30 @@ playerButton.addEventListener("click", () => {
   }
 });
 
-
-
-audio.addEventListener('timeupdate', function progressBar(event) {
-  const {duration, currentTime} = event.target;
+audio.addEventListener("timeupdate", function progressBar(event) {
+  const { duration, currentTime } = event.target;
   const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`
-})
+  progress.style.width = `${progressPercent}%`;
+});
 
-progressContainer.addEventListener('click', function setProgress (event) {
-  const width = this.clientWidth
-  const clickX = event.offsetX
-  const duration = audio.duration
+progressContainer.addEventListener("click", function setProgress(event) {
+  const width = this.clientWidth;
+  const clickX = event.offsetX;
+  const duration = audio.duration;
 
-  audio.currentTime = (clickX / width) * duration
-})
+  audio.currentTime = (clickX / width) * duration;
+});
 
-// switch image
+// switch image and play sound
 
 allButton.addEventListener("click", function changeImage(event) {
+  event.target.dataset.item;
+  mainImg.style.backgroundImage = `url(./assets/img/${event.target.dataset.item}.jpg)`;
+  audio.src = `./assets/audio/${event.target.dataset.item}.mp3`;
+  playAudio();
+});
+
+burgerList.addEventListener("click", function changeImage(event) {
   event.target.dataset.item;
   mainImg.style.backgroundImage = `url(./assets/img/${event.target.dataset.item}.jpg)`;
   audio.src = `./assets/audio/${event.target.dataset.item}.mp3`;

e9a4070f2d2ecf1d30fa0733bc95d90e4f4a54bb
5t4b3r <vitalii.kiktov@gmail.com>
2022-02-06 19:53:13 +0300

    feat: add js and style

---
+++ image-galery/script.js
@@ -0,0 +1,42 @@
+const galleryContainer = document.querySelector(".gallery-container");
+const searchURL = "https://api.unsplash.com/search/photos?query=";
+const requestURL =
+  "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=3XqeEZ0vle1NnQWwuqMIqmsxBrLP1ZgDr_Jep0-vP-c";
+
+
+
+async function getData(requestURL) {
+  const res = await fetch(requestURL);
+  const data = await res.json();
+
+  showData(data);
+}
+getData(requestURL);
+
+function showData(data) {
+  document.querySelector('.gallery-container').innerHTML = '';
+
+  data.results.map((value) => {
+    const img = `<img class="gallery-img" src="${value.links.download}" alt="${value.alt_description}">`;
+    galleryContainer.insertAdjacentHTML("beforeend", img);
+    console.log(data);
+  });
+}
+
+const form = document.querySelector('form');
+const search = document.querySelector('.search');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const apiSearchURL = `${searchURL}${search.value}&per_page=30&orientation=landscape&client_id=3XqeEZ0vle1NnQWwuqMIqmsxBrLP1ZgDr_Jep0-vP-c`
+  if (search.value) {
+    getData(apiSearchURL);
+     
+    search.value = "";
+  }
+  
+  
+
+})
+

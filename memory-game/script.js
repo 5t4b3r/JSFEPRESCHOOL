49ff69104c4b278b75867928e2a89f337fb93c24
5t4b3r <vitalii.kiktov@gmail.com>
2022-02-11 16:32:00 +0300

    feat: add card logic

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,15 +1,33 @@
-const card = document.querySelectorAll('.memory-card');
+const card = document.querySelectorAll('.memory-card')
 
-let hasFlippedCard = false;
-let firstCard, secondCard;
+let hasFlippedCard = false
+let firstCard, secondCard
 
 card.forEach(card => card.addEventListener('click', function flipCard() {
-    card.classList.add('flip')
+    this.classList.add('flip')
+
     if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
+        hasFlippedCard = true
+        firstCard = this
     } else {
-        hasFlippedCard = false;
-        secondCard = this;
+        hasFlippedCard = false
+        secondCard = this
+
+        checkMatch()
     }
+
+    function checkMatch() {
+        if(firstCard.dataset.img === secondCard.dataset.img) {
+            firstCard.removeEventListener('click', flipCard)
+            secondCard.removeEventListener('click', flipCard)
+            } else {
+                setTimeout(() => {
+                    firstCard.classList.remove('flip')
+                    secondCard.classList.remove('flip')
+                }, 1500)
+                
+            }
+    }
+
+   
 }))
